eeb51fe7a73610da04e5bc2505647da43c168ff4
Matt Chesler <matt@chesler.nyc>
2017-06-20 21:43:09 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # route53_dynamic_dns_update
-Simple script to update an A record at Route53 with your current IP
+A simple script to update an A record at Route53 with your current IP.
+
+I run this periodically from cron on a machine at home to update a record in a Route53 hosted zone with the current IP.
+This allows me to always refer to home by a name, and not have to worry about when my ISP changes my IP.
+
+## Installation
+
+Bundler - `bundle install`
+
+## Config
+
+The script requires that you have AWS credentials set, either via environment variables, or `.aws/credentials`.
+See http://docs.aws.amazon.com/sdkforruby/api/index.html#Configuration for more info
+
+## Usage
+
+```
+Usage: update_ip.rb [options]
+    -h, --hostname HOSTNAME          Hostname to update
+    -z, --zone-id ZONE_ID            AWS Route53 Zone ID to update
+    -?, --help                       Display this message
+```
+
+## Run via cron
+
+My crontab for this script looks like:
+```
+AWS_ACCESS_KEY_ID="ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY="SECRET_ACCESS_KEY"
+* * * * * cd /path/to/route53_dynamic_dns_update && bundle exec ./update_ip.rb -h hostname.domain.tld -z hosted_zone_id >> /path/to/log/file.log 2>&1
+```
---
+++ update_ip.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk'
+require 'optparse'
+require 'ostruct'
+
+class UpdateDNS
+  attr_reader :options
+
+  def initialize(args)
+    @options = OpenStruct.new
+    parse(args)
+  end
+
+  def parse(args)
+    opts = OptionParser.new do |opts|
+      opts.on('-h', '--hostname HOSTNAME', 'Hostname to update') do |hostname|
+        options.hostname = hostname
+      end
+
+      opts.on('-z', '--zone-id ZONE_ID', 'AWS Route53 Zone ID to update') do |zone_id|
+        options.zone_id = zone_id
+      end
+
+      opts.on('-?', '--help', 'Display this message') do
+        puts opts
+        exit
+      end
+    end
+
+    opts.banner = "Usage: #{File.basename($PROGRAM_NAME)} [options]"
+
+    begin
+      opts.parse!(args)
+      raise OptionParser::MissingArgument.new("Must provide zone ID") unless options.zone_id
+      raise OptionParser::MissingArgument.new("Must provide hostname") unless options.hostname
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+      puts $!.to_s
+      puts opts
+      exit(1)
+    end
+  end
+
+  def route53
+    @route53 ||= Aws::Route53::Client.new(region: 'us-east-1')
+  end
+
+  def observed_ip
+    @observed_ip ||= `/usr/bin/dig +short myip.opendns.com @resolver1.opendns.com`.chomp
+  end
+
+  def route53_ip
+    @route53_ip ||= begin
+      route53.list_resource_record_sets({
+        hosted_zone_id: options.zone_id,
+        start_record_name: options.hostname,
+        start_record_type: 'A',
+        max_items: 1
+      })
+        .resource_record_sets.first
+        .resource_records.first
+        .value
+    rescue Aws::Route53::Errors::ServiceError, Aws::Errors::MissingCredentialsError => e
+      raise e
+    rescue => e
+      puts e.inspect
+      nil
+    end
+  end
+
+  def update_ip
+    puts "[#{Time.now}] Updating #{options.hostname} from #{route53_ip} to #{observed_ip}"
+    begin
+      route53.change_resource_record_sets({
+        change_batch: {
+          changes: [{
+            action: 'UPSERT',
+            resource_record_set: {
+              name: options.hostname,
+              resource_records: [{
+                value: observed_ip
+              }],
+              ttl: 60,
+              type: 'A'
+            }
+          }]
+        },
+        hosted_zone_id: options.zone_id
+      })
+    rescue Aws::Route53::Errors::ServiceError => e
+      puts "Failed to update IP: #{e.message}"
+    end
+  end
+
+  def go!
+    update_ip unless observed_ip == route53_ip
+  end
+end
+
+UpdateDNS.new(ARGV).go!
+
